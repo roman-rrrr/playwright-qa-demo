1c4721d1bb46d82240cf95b9672cea5c55e61447
roman-rrrr <golromev@icloud.com>
2026-06-09 16:24:02 +0300

    усиленная проверка авторизации по нескольким факторам

--- tests/test_success_login.py
+++ tests/test_success_login.py
@@ -7,4 +7,28 @@ def test_success_login(page):
 
     page.get_by_role("button", name="Login").click()
 
+    print(page.content())
+
     assert "/secure" in page.url
+    
+    assert page.get_by_text("You logged into a secure area!").is_visible()
+
+    assert page.get_by_role("button",name="Logout").is_visible()
+def test_success_login(page):
+    page.goto("https://practice.expandtesting.com/login")
+
+    page.locator("#username").fill("practice")
+    page.locator("#password").fill("SuperSecretPassword!")
+
+    page.get_by_role("button", name="Login").click()
+
+    assert "/secure" in page.url
+
+    assert "You logged into a secure area!" in (
+        page.locator("#flash").text_content()
+    )
+
+    assert page.get_by_role(
+        "link",
+        name="Logout"
+    ).is_visible()
